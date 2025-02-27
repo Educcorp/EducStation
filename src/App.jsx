f704db1f72060ac8837b5c0d8fdb6e8dbb2bf180
Damian <damival.32@gmail.com>
2025-02-26 22:56:31 -0600

    Create App.jsx to conect the pages

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BlogDetailPage from './pages/BlogDetailPage';
+
+// Importar estas páginas después de crearlas:
+// import AboutPage from './pages/AboutPage';
+// import ContactPage from './pages/ContactPage';
+// import CategoryPage from './pages/CategoryPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog/:blogId" element={<BlogDetailPage />} />
+        
+        {/* Descomentar estas rutas después de crear estos componentes: */}
+        {/* <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/category/:categoryName" element={<CategoryPage />} /> */}
+        
+        {/* Ruta de respaldo */}
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
