58edcf23dfe6caa139e935c7091db7692ede433a
Damian <damival.32@gmail.com>
2025-03-14 03:47:37 -0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import AdminPostPage from './pages/AdminPostPage';
 import CategoryPage from './pages/CategoryPage';
-import PostViewer from './components/blog/PostViewer'; // Importamos el nuevo componente
+import PostViewer from './components/blog/PostViewer';
+
+// Nuevas páginas añadidas
+import TermsPage from './pages/TermsPage';
+import PrivacyPage from './pages/PrivacyPage';
+import CookiesPage from './pages/CookiesPage';
 
 const App = () => {
   return (
@@ -17,13 +22,18 @@ const App = () => {
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/blog/:postId" element={<PostViewer />} /> {/* Nueva ruta para visualizar posts */}
-          <Route path="/blog/detail/:blogId" element={<BlogDetailPage />} /> {/* Mantenemos la ruta original */}
+          <Route path="/blog/:postId" element={<PostViewer />} />
+          <Route path="/blog/detail/:blogId" element={<BlogDetailPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/admin/post" element={<AdminPostPage />} />
           <Route path="/admin/post/:postId" element={<AdminPostPage />} />
           <Route path="/category/:categoryName" element={<CategoryPage />} />
+          
+          {/* Nuevas rutas para términos, privacidad y cookies */}
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/cookies" element={<CookiesPage />} />
           
           {/* Ruta de respaldo */}
           <Route path="*" element={<Navigate to="/" replace />} />
