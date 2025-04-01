8de24fe79803249c332cfdfe5f30d01106d30025
Geraldine Justo <juspriscila93@outlook.com>
2025-04-01 10:05:35 -0600

    Conexion exitosa de MySql y Django
    (creditos morales a priscila)
    
    Co-Authored-By: Gregorio Yahir Sánchez Zavaleta  <gregosz3333@gmail.com>

--- Entorno/mi_entorp/mi_entorp/settings.py
+++ Entorno/mi_entorp/mi_entorp/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Educcorp_educs',
         'USER': 'root',
-        'PASSWORD': 'priscila2006',
+        'PASSWORD': 'edugobb575399',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
@@ -86,6 +86,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
 }
 
+            
             
             
     }
