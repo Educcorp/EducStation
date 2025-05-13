cca21eef5f0cb3051bbd6244e8a2e01e7ffc6d85
Yoselin <yreynaga@ucol.mx>
2025-05-13 11:06:11 -0600

    aaaa

--- src/components/blog/PostCard.jsx
+++ src/components/blog/PostCard.jsx
@@ -59,7 +59,7 @@ const PostCard = ({ post }) => {
       color: "white",
       fontSize: typography.fontSize.xs,
       fontWeight: typography.fontWeight.medium,
-      backgroundColor: "#41815f",
+      backgroundColor: "#507c74",
       padding: `${spacing.xs} ${spacing.md}`,
       borderRadius: borderRadius.round,
       textTransform: "capitalize",
